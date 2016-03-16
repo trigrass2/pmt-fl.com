0d779d55ed003afbbc7c514adbd77bcc3042fb96
Bermon Painter <hello@bermonpainter.com>
2016-03-16 07:00:59 -0400

    Fixed navigation, added template pages
    - Made 3rd tier nav full-height to make it easier to get nav items
    - Added missing audio pages

---
+++ audio/phones/_product-index-template.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Time-to-Digital Converters: Precision Measurement Technologies</title>
+  <?php include("../includes/head.php"); ?>
+</head>
+<body>
+
+<!-- BEGIN containerOut -->
+<div id="containerOut">
+
+  <!-- BEGIN containerIN -->
+  <div id="containerIn">
+    <?php include("../includes/header-nav.php"); ?>
+
+
+
+    <section role="main" class="product-index">
+      <aside role="complimentary" class="sidebar">
+        <?php include("../includes/subnav-tdc.php"); ?>    
+      </aside>
+
+      <article class="product-details">
+        <h1 class="primary-heading"></h1>
+        <h2 class="secondary-heading"></h2>
+
+        <p class="into">
+          
+          <img src="" />
+        </p>
+
+        <dl class="product-features">
+          <dt></dt>
+          <dd></dd>
+
+          <dt></dt>
+          <dd></dd>
+        </dl>
+
+        <table class="product-facts">
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+
+      </article>
+    </section>
+    
+      
+    
+    
+    <?php include("../includes/footer.php"); ?>
+  </div>
+  <!-- END containerIN -->
+</div>
+<!-- END containerOut -->
+</body>
+</html>
