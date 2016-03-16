eafad4de32a1be769e3fcd6a73b711283e92dc44
kaityhallman <kaitlinhallman@gmail.com>
2016-03-16 11:05:12 -0400

    as3415, as3420, as3421

--- audio/active-noise-cancelling/as3420.php
+++ audio/active-noise-cancelling/as3420.php
@@ -23,20 +23,28 @@
       </aside>
 
       <article class="product-details">
-        <h1 class="primary-heading"></h1>
-        <h2 class="secondary-heading"></h2>
+        <h1 class="primary-heading">AS3420 Active Noise Cancellation</h1>
+        <h2 class="secondary-heading">Mono bluetooth ANC solution</h2>
 
         <p class="intro">
-          
-          <img src="" />
+          Integrated Active Noise Cancellation solution addressing feedforward and feedback architectures for Mono wireless headsets, such as unified communications solutions and phone applications.
+          <img src="../../images/audio/AS3420-Block-Diagram_f380greyscale.jpg" />
         </p>
 
         <dl class="product-features">
           <dt>Key Features</dt>
-          <dd></dd>
+          <dd>>20dB noise reduction achievable</dd>
+          <dd>High quality audio chain</dd>
+          <dd>Fully electronic calibration</dd>
+          <dd>Differential line inputs</dd>
+          <dd>1.0V - 1.8V supply</dd>
 
           <dt>Key Benefits</dt>
-          <dd></dd>
+          <dd>Superior performance</dd>
+          <dd>Pure, unadulterated audio</dd>
+          <dd>No manual intervention during production</dd>
+          <dd>Seamless connection to BT SoC's</dd>
+          <dd>Optimised system efficiancy</dd>
         </dl>
 
         <table class="product-facts">
@@ -44,8 +52,36 @@
             <th colspan="2">Product Facts</th>
           </tr>
           <tr>
-            <td></td>
-            <td></td>
+            <td>Function</td>
+            <td>Receive path</td>
+          </tr>
+          <tr>
+            <td>Topology</td>
+            <td>Feedforward or feedback</td>
+          </tr>
+          <tr>
+            <td>Output type</td>
+            <td>Mono BTL</td>
+          </tr>
+          <tr>
+            <td>Max output power (BTL, 1.8V, 32 Ohm) [mW]</td>
+            <td>125</td>
+          </tr>
+          <tr>
+            <td>SNR, THD (SE, 34mW, 32Ohm) [dB]</td>
+            <td>>100, <0.1%</td>
+          </tr>
+          <tr>
+            <td>ANC performance [dB]</td>
+            <td>>20</td>
+          </tr>
+          <tr>
+            <td>Supply voltage [V]</td>
+            <td>1.0 to 1.8</td>
+          </tr>
+          <tr>
+            <td>Package</td>
+            <td>QFN-20 (4x4)</td>
           </tr>
         </table>
 
