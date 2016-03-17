96ac6fb6e8201a4c2e18169da9ec4903419c188b
kaityhallman <kaitlinhallman@gmail.com>
2016-03-17 14:40:27 -0400

    added audio-front-ends

--- audio/audio-front-ends/as3543.php
+++ audio/audio-front-ends/as3543.php
@@ -21,26 +21,79 @@
       </aside>
 
       <article class="product-details">
-        <h1 class="primary-heading"></h1>
-        <h2 class="secondary-heading"></h2>
+        <h1 class="primary-heading">AS3543 Audio Codec</h1>
+        <h2 class="secondary-heading">High end stereo audio Codec with power management system</h2>
 
         <p class="intro">
-          
-          <img src="" />
+          High performance, low power, Analog audio front end with integrated dual channel 18bit DAC(102dB SNR, 8-48kHz), 20bit ADC(83dB SNR, 8-24kHz), headphone drive capability and multiple voltage supplies.
+          <img src="../../images/audio/AS3543-Diagram_f380greyscale.jpg" />
         </p>
 
         <dl class="product-features">
-          <dt></dt>
-          <dd></dd>
+          <dt>Key Features</dt>
+          <dd>5mW playback power consumption</dd>
+          <dd>On chip power management</dd>
+          <dd>High performance Codec</dd>
 
-          <dt></dt>
-          <dd></dd>
+          <dt>Key Benefits</dt>
+          <dd>Extend battery life</dd>
+          <dd>Reduces system component count</dd>
+          <dd>Ideal for voice recording and playback</dd>
         </dl>
 
         <table class="product-facts">
           <tr>
+            <th colspan="2">Product Facts</th>
+          </tr>
+          <tr>
+            <td>Power Management</td>
+            <td>"DCDC StepUp: 1x60mA @ 12V<br />
+                      DCDC StepDown: 2x250mA with DVM<br />
+                      LDO: 3x200mA, 1x50mA<br />
+                      Charge Pump: -<br />
+                      Current Sink: 1x36mA (prog., log. Dimming)"</td>
+          </tr>
+          <tr>
+            <td>Main Audio Features</td>
+            <td>"Headphone Amp: 1x<br />
+                    Line In/Out: 1x<br />
+                    Microphone In: 1x<br />
+                    Audio Mix: yes"</td>
+          </tr>
+          <tr>
+            <td>Audio Codec SNR</td>
+            <td>"DAC: 100dB<br />
+                    ADC: 85dB"</td>
+          </tr>
+          <tr>
+            <td>Speaker Amplifier</td>
             <td></td>
-            <td></td>
+          </tr>
+          <tr>
+            <td>Main Interface & Control</td>
+            <td>"Gen. Purpose ADC: 10bit<br />
+                    I2C & I2S: yes<br />
+                    SPDIF: -<br />
+                    RES & WDT: yes<br />
+                    DRM Enabled (UID): 64bit"</td>
+          </tr>
+          <tr>
+            <td>USB Charger with Temp Supervision</td>
+            <td>&#10003;<br />
+            + Current Limitation<br />
+            + Battery Switch</td>   
+          </tr>
+          <tr>
+            <td>Boot ROM for Start-up Sequences</td>
+            <td>5 (25 voltage combinations each)</td>
+          </tr>
+          <tr>
+            <td>RTC</td>
+            <td>&#10003;</td>
+          </tr>
+          <tr>
+            <td>Package</td>
+            <td>BGA-64 (6x6)</td>
           </tr>
         </table>
 
