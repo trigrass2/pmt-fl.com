b4850d833675e4cd2e172d4dd903576acf8ff69b
kaityhallman <kaitlinhallman@gmail.com>
2016-03-30 22:26:26 -0400

    added in wireless connectivity - nfc-afes

---
+++ wireless-connectivity/nfc-afes/as3922.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>NFC AFEs: Precision Measurement Technologies</title>
+  <?php include("../../includes/head.php"); ?>
+</head>
+<body>
+
+<!-- BEGIN containerOut -->
+<div id="containerOut">
+
+  <!-- BEGIN containerIN -->
+  <div id="containerIn">
+    <?php include("../../includes/header-nav.php"); ?>
+
+
+
+    <section role="main" class="product-detail">
+      <article class="product-details">
+        <h1 class="primary-heading">AS3922 Active Load Modulation</h1>
+        <h2 class="secondary-heading">BoostedNFC analog front end for embedded payment and ticketing</h2>
+
+        <p class="intro">
+        The AS3922 is an analog front end with boostedNFC. Designed to sit in front of any dual interface Secure Element. Allowing payment functionality in ultra-small form factors such as SIM and &micro;SD.
+          <img src="../../images/wireless-connectivity/AS3922_Block-Diagram_f380greyscale.jpg" />
+        </p>
+
+        <dl class="product-features">
+          <dt>Key Features</dt>
+          <dd>BoostedNFC (Active Load Modulation)</dd>
+          <dd>Automatic Antenna Tuning</dd>
+          <dd>Automatic Power Adjustment</dd>
+          <dd>ACLB, DCLB and NFC Wi interfaces</dd>
+          <dd>Built in EEPROM</dd>
+
+          <dt>Key Benefits</dt>
+          <dd>Payment functionality in ultra-small form factors</dd>
+          <dd>Tunes for different environments</dd>
+          <dd>Works with lower power readers</dd>
+          <dd>Connects with any dual interface SE</dd>
+          <dd>Programmability for different phone models</dd>
+        </dl>
+
+        <table class="product-facts">
+          <tr>
+            <th colspan="2">Product Facts</th>
+          </tr>
+          <tr>
+            <td>Protocols supported [ ]</td>
+            <td>ISO 14443A/B, FeliCa</td>
+          </tr>
+          <tr>
+            <td>Frequency [ MHz]</td>
+            <td>13.56</td>
+          </tr>
+          <tr>
+            <td>Antenna management [ ]</td>
+            <td>AAT APC</td>
+          </tr>
+          <tr>
+            <td>Supply [ V]</td>
+            <td>2.7 to 3.6</td>
+          </tr>
+          <tr>
+            <td>Sensitivity [mVpp]</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Bitrate [ kbps]</td>
+            <td>106</td>
+          </tr>
+          <tr>
+            <td>Temperature range [&deg;C]</td>
+            <td>-25 to +85</td>
+          </tr>
+          <tr>
+            <td>Package</td>
+            <td>Die</td>
+          </tr>
+        </table>
+
+      </article>
+
+      <aside role="complimentary" class="sidebar">
+        <ul class="vertical-nav">
+          <?php include("../../includes/subnav-wireless-connectivity.php"); ?>    
+        </ul>
+      </aside>
+    </section>
+    
+      
+    
+    
+    <?php include("../../includes/footer.php"); ?>
+  </div>
+  <!-- END containerIN -->
+</div>
+<!-- END containerOut -->
+</body>
+</html>
---
+++ wireless-connectivity/nfc-afes/as39230.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>NFC AFEs: Precision Measurement Technologies</title>
+  <?php include("../../includes/head.php"); ?>
+</head>
+<body>
+
+<!-- BEGIN containerOut -->
+<div id="containerOut">
+
+  <!-- BEGIN containerIN -->
+  <div id="containerIn">
+    <?php include("../../includes/header-nav.php"); ?>
+
+
+
+    <section role="main" class="product-detail">
+      <article class="product-details">
+        <h1 class="primary-heading">AS39230 Active Load Modulationn</h1>
+        <h2 class="secondary-heading">BoostedNFC analog front end for mobile payment and ticketing</h2>
+
+        <p class="intro">
+        ams’ AS39230 boostedNFC IC employs Active Load Modulation to drastically improve NFC performance in challenging environments such as mobile and wearables. By replacing the Analog Front End of existing NFC controllers, AS39230 allows for flexible and low cost design by operating with antenna sizes 20 times smaller than competing solution, or even with metal casing as resonant antenna. Our unique implementation of Active Load Modulation brings NFC user experience to a whole new level, while reducing footprint with no compromise in compliance and interoperability.
+          <img src="../../images/wireless-connectivity/AS39230-block-diagram_f380greyscale.jpg" />
+        </p>
+
+        <dl class="product-features">
+          <dt>Key Features</dt>
+          <dd>ISO14443 type A/B and FeliCa up to 424kb/s</dd>
+          <dd>Active peer to peer bit rates 212kb/s, 424kb/s according to ISO18092</dd>
+          <dd>Digital interface to standard NFC controller or connectivity combo IC</dd>
+          <dd>Automatic output power adjustment</dd>
+
+          <dt>Key Benefits</dt>
+          <dd>EMVCo and ISO compliance with antennas down to 100mm<sup>2</sup> or less</dd>
+          <dd>Considerable reduction in antenna cost, virtually to 0 when metal casing is used</dd>
+          <dd>Best-in-class interoperability over reader infrastructure variances</dd>
+          <dd>NFC Controller agnostic</dd>
+        </dl>
+
+        <table class="product-facts">
+          <tr>
+            <th colspan="2">Product Facts</th>
+          </tr>
+          <tr>
+            <td>Protocols supported [ ]</td>
+            <td>ISO 14443A/B, FeliCa</td>
+          </tr>
+          <tr>
+            <td>Frequency [ MHz]</td>
+            <td>13.56</td>
+          </tr>
+          <tr>
+            <td>Antenna management [ ]</td>
+            <td>AAT APC</td>
+          </tr>
+          <tr>
+            <td>Supply [ V]</td>
+            <td>2.7 to 3.6</td>
+          </tr>
+          <tr>
+            <td>Sensitivity [mVpp]</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Bitrate [ kbps]</td>
+            <td>106</td>
+          </tr>
+          <tr>
+            <td>Temperature range [&deg;C]</td>
+            <td>-25 to +85</td>
+          </tr>
+          <tr>
+            <td>Package</td>
+            <td>Die</td>
+          </tr>
+        </table>
+
+      </article>
+
+      <aside role="complimentary" class="sidebar">
+        <ul class="vertical-nav">
+          <?php include("../../includes/subnav-wireless-connectivity.php"); ?>    
+        </ul>
+      </aside>
+    </section>
+    
+      
+    
+    
+    <?php include("../../includes/footer.php"); ?>
+  </div>
+  <!-- END containerIN -->
+</div>
+<!-- END containerOut -->
+</body>
+</html>
