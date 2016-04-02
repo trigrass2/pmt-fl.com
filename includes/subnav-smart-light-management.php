e3b188912095dffcfcfd1adf1d05bce4f7b94c5d
kaityhallman <kaitlinhallman@gmail.com>
2016-04-02 09:57:18 -0400

    added smart light management - smart lighting products

--- includes/subnav-smart-light-management.php
+++ includes/subnav-smart-light-management.php
@@ -6,9 +6,9 @@
   <ul class="vertical-nav">
     <li class="vertical-nav-item">
       <a href="/smart-light-management/smart-lighting/AS7210.php">AS7210 Standalone daylight sensing and integrated luminaire management</a>
-      <a href="/chemical-sensors/magnets/AS7211.php">AS7211 Network-enabled daylight sensing and integrated IoT luminaire management</a>
-      <a href="/chemical-sensors/magnets/AS7220.php">AS7220 Standalone color/lux sensing and integrated luminaire management</a>
-      <a href="/chemical-sensors/magnets/AS7221.php">AS7221 Network-enabled sensing and integrated IoT luminaire management for spectrally tunable systems</a>
+      <a href="/smart-light-management/smart-lighting/AS7211.php">AS7211 Network-enabled daylight sensing and integrated IoT luminaire management</a>
+      <a href="/smart-light-management/smart-lighting/AS7220.php">AS7220 Standalone color/lux sensing and integrated luminaire management</a>
+      <a href="/smart-light-management/smart-lighting/AS7221.php">AS7221 Network-enabled sensing and integrated IoT luminaire management for spectrally tunable systems</a>
     </li>
   </ul>
 </li>
