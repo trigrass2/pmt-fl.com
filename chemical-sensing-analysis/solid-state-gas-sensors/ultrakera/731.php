0578a92ad3daa7bd96329183b9bba32f169359e7
kaityhallman <kaitlinhallman@gmail.com>
2016-04-07 18:11:25 -0400

    added in ultrakera line products

---
+++ chemical-sensing-analysis/solid-state-gas-sensors/ultrakera/731.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>UltraKera&#0153; TO - Ultra Low-Power Microsensors - Solid State Gas Sensors - Chemical Sensing Analysis: Precision Measurement Technologies</title>
+  <?php include("../../../includes/head.php"); ?>
+</head>
+<body>
+
+<!-- BEGIN containerOut -->
+<div id="containerOut">
+
+  <!-- BEGIN containerIN -->
+  <div id="containerIn">
+    <?php include("../../../includes/header-nav.php"); ?>
+
+
+
+    <section role="main" class="product-detail">
+      <article class="product-details">
+        <h1 class="primary-heading">731 UltraKera&#0153; TO VOC Sensor</h1>
+
+        <p class="intro">
+          <img src="../../../images/chemical-sensing-analysis/731-ultrakera-to-co-sensor.png" />
+        </p>
+
+        <dl class="product-features">
+          <dt>Sensor Features</dt>
+          <dd>New low power designn: 40 mA @ 1 V</dd>
+          <dd>Innovative chemiresistor technology</dd>
+          <dd>Environmental temperature range of -40 to 60&deg;C with appropriate heater contro</dd>
+          <dd>Thermistor heater allows active control of sensor temperature based on environmental temperature</dd>
+          <dd>Environmental humidity range of 0 to 95% RH, noncondensing</dd>
+          <dd>Sensor packaged on low profile TO-46 header</dd>
+        </dl>
+
+        <table class="product-facts">
+          <tr>
+            <th colspan="2">Electrical Characteristics</th>
+          </tr>
+          <tr>
+            <td><b>Property</b></td>
+            <td><b>Symbol</b></td>
+            <td><b>Value</b></td>
+            <td><b>Remarks</b></td>
+          </tr>
+          <tr>
+            <td>Heater Power Consumption</td>
+            <td>P<sub>H</sub></td>
+            <td>40 mW</td>
+            <td>Continuous at V<sub>H</sub> = 1.0V</td>
+          </tr>
+          <tr>
+            <td>Heater Voltage</td>
+            <td>V<sub>H</sub></td>
+            <td>1.0 VDC</td>
+            <td>T<sub>sensor</sub> ~220&deg;C,<br />
+            R<sub>H</sub> = 16&#937;</td>
+          </tr>
+          <tr>
+            <td>Heater Resistance</td>
+            <td>R<sub>H</sub></td>
+            <td>14 to 18 Ohms</td>
+            <td>At room temperature</td>
+          </tr>
+          <tr>
+            <td>Sensing Voltage</td>
+            <td>V<sub>C</sub></td>
+            <td>2.5 VDC</td>
+            <td>Typical</td>
+          </tr>
+          <tr>
+            <td>Typical Resistance in Air</td>
+            <td>R<sub>a</sub></td>
+            <td>200 k&#937;/10 M&#937;*</td>
+            <td>Min/Max</td>
+          </tr>
+          <tr>
+            <td>Typical Resistance in 500 ppm Ethanol<sub>4</sub></td>
+            <td>R<sub>1.0</sub></td>
+            <td>1 k&#937;*/200 k&#937;*</td>
+            <td>Min/Max</td>
+          </tr>
+          <tr>
+            <td>Typical Sensitivity</td>
+            <td>R<sub>a</sub>/R<sub>500</sub></td>
+            <td>10</td>
+            <td>Min</td>
+          </tr>
+          <tr>
+            <td>Repeatability</td>
+            <td></td>
+            <td>&plusmn;5% Full Scale/&plusmn;10% Reading</td>
+            <td>Whichever is Greater</td>
+          </tr>
+        </table>
+
+      </article>
+
+      <aside role="complimentary" class="sidebar">
+        <ul class="vertical-nav">
+          <?php include("../../../includes/subnav-chemical-sensing-analysis.php"); ?>    
+        </ul>
+      </aside>
+    </section>
+    
+      
+    
+    
+    <?php include("../../../includes/footer.php"); ?>
+  </div>
+  <!-- END containerIN -->
+</div>
+<!-- END containerOut -->
+</body>
+</html>
