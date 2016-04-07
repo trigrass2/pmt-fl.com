9948a0ff3bf0f294953e964c1f0b5143d18ffbd4
kaityhallman <kaitlinhallman@gmail.com>
2016-04-06 20:33:39 -0400

    added in chemical sensing analysis

---
+++ includes/subnav-chemical-sensing-analysis.php
@@ -0,0 +1,35 @@
+<li class="vertical-nav-item vertical-nav-heading">
+  Chemical Sensing & Analysis
+</li>
+<li class="vertical-nav-item">
+  <a href="/chemical-sensing-analysis/solid-state-gas-sensors/prokera/" data-dropdown="right">ProKera&#0153; line</a>
+  <ul class="vertical-nav">
+    <li class="vertical-nav-item">
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/prokera/701.php">701 Trace Hydrogen Sensor</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/prokera/705.php">701 Ammonia Sensor</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/prokera/706.php">706 Low Level NO Data Sheet</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/prokera/707.php">707 VOC Sensor</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/prokera/711.php">711 Flammable Gas Sensor</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/prokera/714.php">714 Hydrogen Sulfide (H2S) Sensor</a>
+    </li>
+  </ul>
+</li>
+<li class="vertical-nav-item">
+  <a href="/chemical-sensing-analysis/solid-state-gas-sensors/mikrokera/" data-dropdown="right">MikroKera&#0153; line</a>
+  <ul class="vertical-nav">
+    <li class="vertical-nav-item">
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/mikrokera/724.php">724 MikroKera 4L Hydrogen Sensor</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/mikrokera/727.php">727 MikroKera 4L Hydrogen Sulfide Sensor</a>
+    </li>
+  </ul>
+</li>
+<li class="vertical-nav-item">
+  <a href="/chemical-sensing-analysis/solid-state-gas-sensors/ultrakera/" data-dropdown="right">Ultra&#0153; line</a>
+  <ul class="vertical-nav">
+    <li class="vertical-nav-item">
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/ultrakera/729.php">729 UltraKera&#0153; TO IS Methane Sensor</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/ultrakera/730.php">730 UltraKera&#0153; TO Carbon Monoxide (CO) Sensor</a>
+      <a href="/chemical-sensing-analysis/solid-state-gas-sensors/ultrakera/731.php">731 UltraKera&#0153; TO VOC Sensor</a>
+    </li>
+  </ul>
+</li>
