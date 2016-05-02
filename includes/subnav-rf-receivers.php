5329000eb20d54d2d2562a95d36f9aacfcdc9e4f
Micah Ovadia <movadia@CSGCINMAC38TEST.local>
2016-05-02 07:49:06 -0400

    Remove AS3955 from rf-transmitters-receivers section, add Overview to Lighting Sensors

--- includes/subnav-rf-receivers.php
+++ includes/subnav-rf-receivers.php
@@ -8,9 +8,6 @@
 			<a href="/rf-transmitters-receivers/lf-receivers/as3930.php">AS3930 Programmable 1D LF wake-up receiver</a>
 			<a href="/rf-transmitters-receivers/lf-receivers/as3932.php">AS3932 Programmable 3D LF wake-up receiver</a>
 			<a href="/rf-transmitters-receivers/lf-receivers/as3933.php">AS3933 Programmable 3D LF wake-up receiver</a>
-			<a href="/rf-transmitters-receivers/lf-receivers/as3955.php">AS3955 NFC to SPI/I2C interface NFC Forum Compliant Dynamic Tag
-
-</a>
 		</li>
 	</ul>
 </li>
